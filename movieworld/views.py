2a576c220c1ca76034163ce7defaa08bffd53f0e
Arty Barnabi <rbarnabi676@gmail.com>
2020-02-11 09:28:32 -0800

    added views for all list

--- movieworld/views.py
+++ movieworld/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Movie, Director
+from .forms import DirectorForm, MovieForm
+
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'tunr/artist_list.html', {'artists': artists})
+
+def director_list(request):
+    directors = Director.objects.all()
+    return render(request, 'movieworld/director_list.html', {'directors': directors})
