befb3a49c68216743ce3c5618dafd7f39d4fe922
MK16681 <KhanMuhammadT@JohnDeere.com>
2020-02-11 12:17:05 -0800

    changes

--- movieworld/models.py
+++ movieworld/models.py
@@ -7,14 +7,13 @@ class Director(models.Model):
     nationality = models.CharField(max_length=100)
     birthday = models.TextField()
     def __str__(self):
-        return self.name
+        return self.director_name
 
 class Movie(models.Model):
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='name')
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='director_names')
     name = models.CharField(max_length=100, default='no name')
     genre = models.CharField(max_length=100, default='no genre')
     release_date = models.CharField(max_length=200, null=True)
-    director_name= models.CharField(max_length=200, null=True)
     stars= models.CharField(max_length=200, null=True)
     def __str__(self):
         return self.name
