2388b12b7479c8309097ad3239d546170f995ba2
ashea27 <shea.andrew27@gmail.com>
2020-02-12 09:16:09 -0700

    remove .env and .vscode from github

--- .env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ashea29/Documents/sei/projects/python_project/python-project/.env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
